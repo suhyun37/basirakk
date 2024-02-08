829b0fb238e9556db30fc1305381bae317df1b1b
suhyun37 <chltngus234@snu.ac.kr>
2024-02-08 20:40:11 +0900

    first comit

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, "homepage/index.html")
+    
+def about(request):
+    return render(request, "about.html")
+    
+def post(request):
+    return render(request, "post.html")
+
 # Create your views here.
